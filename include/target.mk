af76a39d911d75bbdf49f26dd97715a9a677816d
Tianling Shen <cnsztl@immortalwrt.org>
2022-03-13 15:09:23 +0800

    Revert "target: switch to firewall4"
    This is not ready yet.
    
    This reverts commit a1a97d28c2bcf55c35a657b5623b2516113fe0a7.
    
    Signed-off-by: Tianling Shen <cnsztl@immortalwrt.org>

--- include/target.mk
+++ include/target.mk
@@ -54,11 +54,9 @@ DEFAULT_PACKAGES.nas:=\
 # For router targets
 DEFAULT_PACKAGES.router:=\
 	dnsmasq-full \
-	firewall4 \
-	nftables \
-	kmod-nft-offload \
-	odhcp6c \
-	odhcpd-ipv6only \
+	firewall \
+	iptables-zz-legacy \
+	kmod-ipt-offload \
 	ppp \
 	ppp-mod-pppoe
 # For easy usage
@@ -66,9 +64,8 @@ DEFAULT_PACKAGES.tweak:=\
 	autocore \
 	block-mount \
 	default-settings-chn \
-	iptables-nft \
-	ip6tables-nft \
 	ipv6helper \
+	kmod-ipt-raw \
 	kmod-nf-nathelper \
 	kmod-nf-nathelper-extra \
 	luci \
@@ -110,7 +107,7 @@ else
 endif
 
 # Add tweaked packages
-# DEFAULT_PACKAGES += $(DEFAULT_PACKAGES.tweak)
+DEFAULT_PACKAGES += $(DEFAULT_PACKAGES.tweak)
 
 # Add device specific packages (here below to allow device type set from subtarget)
 DEFAULT_PACKAGES += $(DEFAULT_PACKAGES.$(DEVICE_TYPE))
